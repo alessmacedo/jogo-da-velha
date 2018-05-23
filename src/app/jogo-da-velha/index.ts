8f6ca49adb78fc0e5ba69386ddb204bbc9b1da19
Alessandro Macedo <alessmacedo.ti@gmail.com>
2018-05-23 18:54:59 -0300

    Implementado: index.ts

--- src/app/jogo-da-velha/index.ts
+++ src/app/jogo-da-velha/index.ts
@@ -1,1 +1,5 @@
+/*
+* Criação do arquivo Index.ts para facilitar a importação dos dados.
+*/
 export * from './jogo-da-velha.module';
+export * from './jogo-da-velha.component'; // Adicionado Component
